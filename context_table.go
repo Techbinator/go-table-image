142837a0b4d562981ca6e85f6e25a44caab2d6f9
Tudor Filipovici <tudor.filipovici@holidaycheck.com>
2019-08-21 15:42:03 +0200

    Draw column separator line

--- context_table.go
+++ context_table.go
@@ -9,11 +9,13 @@ import (
 func (ti *tableImage) drawTH() {
 	for colNo, td := range ti.th.Tds {
 		ti.addString(colNo*ti.columnSpace+tablePadding, 1*rowSpace, td.Text, td.Color)
+		ti.addLine(colNo*ti.columnSpace, 0, colNo*ti.columnSpace, ti.height, "#000000")
 	}
 
 	//draw the double line to signal it is a th
 	ti.addLine(1, 1*rowSpace+separatorPadding, ti.width, 1*rowSpace+separatorPadding, "#000000")
 	ti.addLine(1, 1*rowSpace+separatorPadding+2, ti.width, 1*rowSpace+separatorPadding+2, "#000000")
+
 }
 
 func (ti *tableImage) drawTR() {
