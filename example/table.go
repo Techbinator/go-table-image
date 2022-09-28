f0c15f1fc9db5e7ad528bcabb26e3953e9b46807
vucchaid <91983332+vucchaid@users.noreply.github.com>
2022-09-28 22:25:50 +0530

    fix import issue

--- example/table.go
+++ example/table.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"tableimage"
+	tableimage "github.com/Techbinator/go-table-image"
 )
 
 func main() {
@@ -11,15 +11,15 @@ func main() {
 		tableimage.TR{
 			BorderColor: "#000",
 			Tds: []tableimage.TD{
-				tableimage.TD{
+				{
 					Color: "#000",
 					Text:  "Id",
 				},
-				tableimage.TD{
+				{
 					Color: "#000",
 					Text:  "Name",
 				},
-				tableimage.TD{
+				{
 					Color: "#008000",
 					Text:  "Price",
 				},
@@ -29,52 +29,52 @@ func main() {
 
 	ti.AddTRs(
 		[]tableimage.TR{
-			tableimage.TR{
+			{
 				BorderColor: "#000",
 				Tds: []tableimage.TD{
-					tableimage.TD{
+					{
 						Color: "#000",
 						Text:  "2223",
 					},
-					tableimage.TD{
+					{
 						Color: "#000",
 						Text:  "Really cool product on two lines",
 					},
-					tableimage.TD{
+					{
 						Color: "#0000ff",
 						Text:  "2000$",
 					},
 				},
 			},
-			tableimage.TR{
+			{
 				BorderColor: "#000",
 				Tds: []tableimage.TD{
-					tableimage.TD{
+					{
 						Color: "#000",
 						Text:  "11",
 					},
-					tableimage.TD{
+					{
 						Color: "#000",
 						Text:  "A more cooler product this time on 3 lines",
 					},
-					tableimage.TD{
+					{
 						Color: "#0000ff",
 						Text:  "200$",
 					},
 				},
 			},
-			tableimage.TR{
+			{
 				BorderColor: "#000",
 				Tds: []tableimage.TD{
-					tableimage.TD{
+					{
 						Color: "#000",
 						Text:  "2231",
 					},
-					tableimage.TD{
+					{
 						Color: "#000",
 						Text:  "Lenovo",
 					},
-					tableimage.TD{
+					{
 						Color: "#000",
 						Text:  "20400$",
 					},
